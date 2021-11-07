95b6ebe60f7fadcc3ec9fda7767b4326a18d076e
dimkich86@gmail.com <dimkich86@gmail.com>
2021-11-07 15:24:38 +0300

    play sound

--- poc/CustomAlarm.MAUI/CustomAlarm.MAUI/Platforms/Android/MyAlarmReceiver.cs
+++ poc/CustomAlarm.MAUI/CustomAlarm.MAUI/Platforms/Android/MyAlarmReceiver.cs
@@ -1,4 +1,5 @@
 ﻿using Android.Content;
+using Android.Media;
 using Android.Widget;
 
 namespace CustomAlarm.MAUI;
@@ -9,6 +10,14 @@ internal class MyAlarmReceiver : BroadcastReceiver
     public override void OnReceive(Context context, Intent intent)
     {
         var requestCode = intent.GetIntExtra("REQUEST_CODE", -1);
+        PlayRingtone(context);
         Toast.MakeText(context, $"Alarm fired with request code {requestCode}", ToastLength.Short).Show();
+    }
+
+    private static void PlayRingtone(Context context)
+    {
+        var soundUri = RingtoneManager.GetDefaultUri(RingtoneType.Alarm);
+        var ringtone = RingtoneManager.GetRingtone(context, soundUri);
+        ringtone.Play();
     }
 }
