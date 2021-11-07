6cc13dc3a107314d39ca054af79ebcbe3a9fa899
dimkich86@gmail.com <dimkich86@gmail.com>
2021-11-07 14:44:48 +0300

    groom the code

--- poc/CustomAlarm.MAUI/CustomAlarm.MAUI/Platforms/Android/MainActivity.cs
+++ poc/CustomAlarm.MAUI/CustomAlarm.MAUI/Platforms/Android/MainActivity.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
 using Android.App;
@@ -14,9 +13,9 @@ using Microsoft.Maui;
 
 namespace CustomAlarm.MAUI
 {
-	[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
-	public class MainActivity : MauiAppCompatActivity
-	{
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : MauiAppCompatActivity
+    {
         private readonly IGeneralEventsController _generalEventsController;
         private readonly IDisposable _onSetAlarmSubscription;
 
@@ -25,38 +24,50 @@ namespace CustomAlarm.MAUI
             _generalEventsController = App.ServiceProvider.GetService<IGeneralEventsController>();
             _onSetAlarmSubscription = Observable
               .FromEventPattern(_generalEventsController, nameof(IGeneralEventsController.OnSetAlarmClickedEvent))
-              .Subscribe(x => SetAlarm(1));
+              .Subscribe(x => SetAlarm());
         }
 
-        private void SetAlarm(int number)
+        private void SetAlarm()
         {
             var simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+            var calendarItem = GetCalendarItem();
+            var requestCode = (int)calendarItem.TimeInMillis / 1000;
+            var alarmIntent = GetAlarmIntent(requestCode);
+
+            SetAlarm(calendarItem, alarmIntent);
+
+            var textTimer = new StringBuilder("Alarm has been set")
+                .AppendLine()
+                .Append(simpleDateFormat.Format(calendarItem.TimeInMillis))
+                .AppendLine();
+
+            Toast.MakeText(this, textTimer.ToString(), ToastLength.Short).Show();
+        }
+
+        private static Calendar GetCalendarItem()
+        {
+            var calendarItem = Calendar.Instance;
+            calendarItem.Add(CalendarField.Second, 10);
+            return calendarItem;
+        }
+
+        private void SetAlarm(Calendar calendarItem, PendingIntent alarmIntent)
+        {
             var am = GetSystemService(AlarmService) as AlarmManager;
-            var calendar = Calendar.Instance;
-            var calendarList = Enumerable.Range(1, number).Select(x => calendar).ToList();
-            var textTimer = new StringBuilder("Alarm has been set").AppendLine();
-            foreach (var calendarItem in calendarList)
-            {
-                calendarItem.Add(CalendarField.Second, 10);
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+                am.SetExactAndAllowWhileIdle(AlarmType.RtcWakeup, calendarItem.TimeInMillis, alarmIntent);
+            else
+                am.SetExact(AlarmType.RtcWakeup, calendarItem.TimeInMillis, alarmIntent);
 
-                var requestCode = (int)calendar.TimeInMillis / 1000;
-                var intent = new Intent(this, typeof(MyAlarmReceiver));
-                intent.PutExtra("REQUEST_CODE", requestCode);
-                intent.AddFlags(ActivityFlags.IncludeStoppedPackages);
-                intent.AddFlags(ActivityFlags.ReceiverForeground);
-                var pi = PendingIntent.GetBroadcast(this, requestCode, intent, 0);
-                if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
-                    am.SetExactAndAllowWhileIdle(AlarmType.RtcWakeup, calendarItem.TimeInMillis, pi);
-                else
-                    am.SetExact(AlarmType.RtcWakeup, calendarItem.TimeInMillis, pi);
+        }
 
-                textTimer
-                    .Append(simpleDateFormat.Format(calendarItem.TimeInMillis))
-                    .AppendLine();
-            }
-
-            string value = textTimer.ToString();
-            Toast.MakeText(this, value, ToastLength.Short).Show();
+        private PendingIntent GetAlarmIntent(int requestCode)
+        {
+            var intent = new Intent(this, typeof(MyAlarmReceiver));
+            intent.PutExtra("REQUEST_CODE", requestCode);
+            intent.AddFlags(ActivityFlags.IncludeStoppedPackages);
+            intent.AddFlags(ActivityFlags.ReceiverForeground);
+            return PendingIntent.GetBroadcast(this, requestCode, intent, 0);
         }
 
         protected override void Dispose(bool disposing)
