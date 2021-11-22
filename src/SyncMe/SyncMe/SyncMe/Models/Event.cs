c64d7c54c741754c1f3d84551fda900bc4b4fde6
Mikhail Fedorov <imikhail.waq@gmail.com>
2021-11-22 13:01:03 +0300

    update models

--- src/SyncMe/SyncMe/SyncMe/Models/Event.cs
+++ src/SyncMe/SyncMe/SyncMe/Models/Event.cs
@@ -1,6 +1,11 @@
 ﻿namespace SyncMe.Models;
 
-public record Event(int Id, string Title, string Description, Namespace Namespace, Schedule Schedule, Alert Alert);
+public enum Status
+{
+    Active, Stopped
+}
+
+public record Event(int Id, string Title, string Description, Namespace Namespace, Schedule Schedule, Alert Alert, Status Status);
 
 public record NamespaceTree(ILookup<Namespace, Namespace> Tree);
 
@@ -8,9 +13,14 @@ public record Namespace(int Id, string Title);
 
 public enum Repeat
 {
-    Dayly, WeekDays
+    Dayly, WeekDays, EveryMonth, EveryYear
 }
 
-public record Schedule();
+public record Schedule(Repeat Repeat);
 
-public record Alert();
+public record Alert(Reminder[] Reminders);
+
+public enum Reminder
+{
+    AtEventTime, Before1Hour, Before30Min
+}
