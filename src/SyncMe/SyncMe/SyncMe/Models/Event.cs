3cbc3e5faab9671e74a5bb33f09353352596cd60
dimkich86@gmail.com <dimkich86@gmail.com>
2021-11-22 12:39:12 +0300

    temp models

---
+++ src/SyncMe/SyncMe/SyncMe/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿namespace SyncMe.Models;
+
+public record Event(int Id, string Title, string Description, Namespace Namespace, Schedule Schedule, Alert Alert);
+
+public record NamespaceTree(ILookup<Namespace, Namespace> Tree);
+
+public record Namespace(int Id, string Title);
+
+public enum Repeat
+{
+    Dayly, WeekDays
+}
+
+public record Schedule();
+
+public record Alert();
