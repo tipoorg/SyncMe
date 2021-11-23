3d48e34de2088cce8442f323aa0e26bb83569029
Nikolai_Kuksov <Nikolai.Kuksov.ext@hoffmann-group.com>
2021-11-24 00:21:28 +0300

    Design improved

--- src/SyncMe/SyncMe/SyncMe/Models/NamespaceModel.cs
+++ src/SyncMe/SyncMe/SyncMe/Models/NamespaceModel.cs
@@ -1,14 +1,24 @@
-﻿using System.Reactive.Subjects;
+﻿using System.ComponentModel;
+using System.Reactive.Subjects;
 using System.Windows.Input;
 
 namespace SyncMe.Models
 {
-    public class NamespaceModel
+    public class NamespaceModel : INotifyPropertyChanged
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public bool IsVisible { get; set; }
+        private bool _isButtonsVisible;
+        public bool IsButtonsVisible
+        {
+            get { return _isButtonsVisible; }
+            set 
+            {
+                _isButtonsVisible = value;
+                OnPropertyChanged(nameof(IsButtonsVisible));
+            }
+        }
 
         public bool IsActive { get; set; }
 
@@ -24,6 +34,16 @@ namespace SyncMe.Models
             Id = id;
             TomorrowClick = new Command(() => TomorrowClicked.OnNext(this));
             RestoreClick = new Command(() => RestoreClicked.OnNext(this));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
         }
     }
 }
