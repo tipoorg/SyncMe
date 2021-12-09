57465f9ec52bcaccac0cd46c13914e36b00c110d
Mikhail Fedorov <imikhail.waq@gmail.com>
2021-12-09 13:08:32 +0300

    #extracted shared app icons

---
+++ src/SyncMe/SyncMe.Lib/MarkupExtensions/ImageExtension.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace SyncMe.Lib.MarkupExtensions;
+
+internal class ImageExtension : IMarkupExtension<ImageSource>
+{
+    public string SharedSource { get; set; }
+
+    public ImageSource ProvideValue(IServiceProvider serviceProvider) => 
+        SharedSource == null ? null : ImageSource.FromResource($"SyncMe.Lib.Resources.{SharedSource}");
+
+    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+    {
+        return ProvideValue(serviceProvider);
+    }
+}
