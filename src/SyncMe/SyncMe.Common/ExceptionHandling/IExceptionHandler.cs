d699d3fcbf75fa15670e12636e29b762b11d5598
Mikhail Fedorov <imikhail.waq@gmail.com>
2022-02-05 20:11:42 +0300

    #global_handling: added global exception handler for both platforms

---
+++ src/SyncMe/SyncMe.Common/ExceptionHandling/IExceptionHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace SyncMe.ExceptionHandling;
+
+public interface IExceptionHandler
+{
+    void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs);
+    void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs);
+}
